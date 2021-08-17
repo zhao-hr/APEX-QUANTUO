c3aaf1462a7ce94e623cf304fa486b1fa4f428dc
zhao-hr <49093503+zhao-hr@users.noreply.github.com>
2021-08-17 19:41:03 +0800

    Update train_gbdt.py

--- train_gbdt.py
+++ train_gbdt.py
@@ -1,21 +1,20 @@
 import sklearn
 from datasets import MyDataSet
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
 from sklearn import metrics
 import joblib
 import os
 
-LABEL = 'total_sales'
+LABEL = 'total_tax'
 DATA_PATH = 'data/final_table.csv'
 CHECKPOINT_PATH = 'stats/gbdt'
 NEW_MODEL_FLAG = True   ### Whether to train a new model instead of loading checkpoint.pkl
 
-model = GradientBoostingClassifier(learning_rate=0.1, subsample=0.9, n_estimators=100, max_depth=5, min_samples_leaf=2)
+model = GradientBoostingRegressor(learning_rate=0.1, subsample=1, n_estimators=100)
 train_dataset = MyDataSet(DATA_PATH, LABEL, 1)
 test_dataset = MyDataSet(DATA_PATH, LABEL, 0)
 X_train, y_train = train_dataset.get_data()
 X_test, y_test = test_dataset.get_data()
-print(X_train.shape, X_test.shape)
 len_train = X_train.shape[0]
 len_test = X_test.shape[0]
 
@@ -33,23 +32,9 @@ def train():
 
 def test():
     predicted = model.predict(X_test)
-    TP = 0
-    FP = 0
-    FN = 0
-    TN = 0
-    for i in range(len_test):
-        #print(y_test[i], predicted[i])
-        if y_test[i] == 1:
-            if predicted[i] == 1: TP += 1
-            else: FN += 1
-        else:
-            if predicted[i] == 1: FP += 1
-            else: TN += 1
-    TPRate = TP / (TP + FN)
-    FPRate = FP / (FP + TN)
+
     AUC = metrics.roc_auc_score(y_test, predicted)
-    print('TPRate: ', TPRate)
-    print('FPRate: ', FPRate)
+
     print('AUC: ', AUC)
     
 
